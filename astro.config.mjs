cbb6ec3582d8b09609b828928b57c927e8133ee9
Nishant Kumar <nishantkumar951@github.com>
2026-06-26 21:42:50 +0530

    Add Astro Sitemap integration and robots.txt file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.vpinfra.co.in/',
+  integrations: [sitemap()],
   // Using default 'directory' format so that clean URLs like /about-us 
   // correctly map to /about-us/index.html on all hosting providers.
 });
