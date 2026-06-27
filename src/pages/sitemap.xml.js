67b2ea43772878a2aba9046a76487a4c95c35ba5
Nishant Kumar <nishantkumar951@github.com>
2026-06-27 09:03:16 +0530

    Create three detailed 1000-word blog post subpages and update links/sitemap

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -5,7 +5,10 @@ export async function GET() {
     'https://vpinfra.co.in/contact-us/',
     'https://vpinfra.co.in/services/building-construction-company-in-lucknow/',
     'https://vpinfra.co.in/services/solar-installation/',
-    'https://vpinfra.co.in/blog/'
+    'https://vpinfra.co.in/blog/',
+    'https://vpinfra.co.in/blog/pm-surya-ghar-yojna-lucknow/',
+    'https://vpinfra.co.in/blog/building-construction-trends-lucknow/',
+    'https://vpinfra.co.in/blog/solar-energy-business-up-investment/'
   ];
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
