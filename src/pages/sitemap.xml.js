1f7f52d5e0c8c8e22f0db5471dce481487ca577f
Nishant Kumar <nishantkumar951@github.com>
2026-06-29 18:06:16 +0530

    Standardise canonical URLs, sitemap, and configurations to prefer www subdomain

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,14 +1,14 @@
 export async function GET() {
   const pages = [
-    'https://vpinfra.co.in/',
-    'https://vpinfra.co.in/about-us/',
-    'https://vpinfra.co.in/contact-us/',
-    'https://vpinfra.co.in/services/building-construction-company-in-lucknow/',
-    'https://vpinfra.co.in/services/solar-installation/',
-    'https://vpinfra.co.in/blog/',
-    'https://vpinfra.co.in/blog/pm-surya-ghar-yojna-lucknow/',
-    'https://vpinfra.co.in/blog/building-construction-trends-lucknow/',
-    'https://vpinfra.co.in/blog/solar-energy-business-up-investment/'
+    'https://www.vpinfra.co.in/',
+    'https://www.vpinfra.co.in/about-us/',
+    'https://www.vpinfra.co.in/contact-us/',
+    'https://www.vpinfra.co.in/services/building-construction-company-in-lucknow/',
+    'https://www.vpinfra.co.in/services/solar-installation/',
+    'https://www.vpinfra.co.in/blog/',
+    'https://www.vpinfra.co.in/blog/pm-surya-ghar-yojna-lucknow/',
+    'https://www.vpinfra.co.in/blog/building-construction-trends-lucknow/',
+    'https://www.vpinfra.co.in/blog/solar-energy-business-up-investment/'
   ];
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
